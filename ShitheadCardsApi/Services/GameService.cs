4d56511b06f70e07b8b6654221d52a12b426b919
Mauro Schaparini <Mauro.Schaparini@thomsonreuters.com>
2020-05-17 13:12:30 +0100

    add all cards

--- ShitheadCardsApi/Services/GameService.cs
+++ ShitheadCardsApi/Services/GameService.cs
@@ -17,6 +17,8 @@ namespace ShitheadCardsApi
             _ctx = ctx;
         }
 
+        List<string> allCards = new List<string>(){ "2H", "2D", "2C", "2S", "3H", "3D", "3C", "3S", "4H", "4D", "4C", "4S", "5H", "5D", "5C", "5S", "6H", "6D", "6C", "6S", "7H", "7D", "7C", "7S", "8H", "8D", "8C", "8S", "9H", "9D", "9C", "9S", "0H", "0D", "0C", "0S", "JH", "JD", "JC", "JS", "QH", "QD", "QC", "QS", "KH", "KD", "KC", "KS", "AH", "AD", "AC", "AS" };
+
         public void CreateGame()
         {
             var game = new Game
