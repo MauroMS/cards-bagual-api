5fc2d732b382311ab1dda02d94619cdf00e8a6e6
Mauro Schaparini <mauro.schaparini@gmail.com>
2020-05-22 20:38:52 +0100

    Add testes

--- ShitheadCardsApi.Moq/ShitheadServiceTests.cs
+++ ShitheadCardsApi.Moq/ShitheadServiceTests.cs
@@ -148,26 +148,15 @@ namespace ShitheadCardsApi.Moq
         }
 
 
-        // No Cards on the table - Return OK
-        // No Cards on the table + 4 of the same number - Return OkBurned
-        // Lower card than the one on the table - Return Refuse (Other than 2/3/10)
-        // Higher card than the one on the table - Return Ok
-        // Single same card as the one on the table (Not 4 of the same) - Return Ok
-        // Two card the same as the one on the table (3 in total) - Return 
-        // Three card the same as the one on the table (4 in total) - Return OkBurned
-        // Play 2 - Return Ok
-        // Play 3 - Return Ok
-        // Play Higher card when there is a 7 on the table - Return Refuse
-        // Play Lower card when there is a 7 on the table - Return Ok
-        // Play 10
-
         [Fact]
         public void EvaluateCardsOnTable_ShouldReturnResultOk_NoCardsOnTheTable()
         {
             // Arrange
+            List<string> cardsToBePlayed = new List<string>() { "2A" };
+            List<string> tableCards = new List<string>() { };
 
             // Act
-            var discardResult = _sut.EvaluateCardsOnTable(new List<string>() { "2A" }, new List<string>());
+            var discardResult = _sut.EvaluateCardsOnTable(cardsToBePlayed, tableCards);
 
             // Assert
             Assert.True(discardResult == DiscardResult.Ok);
@@ -176,104 +165,237 @@ namespace ShitheadCardsApi.Moq
         [Fact]
         public void EvaluateCardsOnTable_ShouldReturnResultOkBurned_NoCardsOnTheTable4OfTheSame() {
             // Arrange
+            List<string> cardsToBePlayed = new List<string>() { "2A", "2H", "2C", "2S" };
+            List<string> tableCards = new List<string>() { };
 
             // Act
-            var discardResult = _sut.EvaluateCardsOnTable(new List<string>() { "2A", "2H", "2C", "2S" }, new List<string>());
+            var discardResult = _sut.EvaluateCardsOnTable(cardsToBePlayed, tableCards);
 
             // Assert
             Assert.True(discardResult == DiscardResult.OkBurned);
-
         }
 
-
         [Fact]
-        public void EvaluateCardsOnTable_ShouldReturnResultRefuse_CardNotValidToPlay() {
+        public void EvaluateCardsOnTable_ShouldReturnResultRefuse_LowerCardThanTableTop() {
             // Arrange
-
+            List<string> cardsToBePlayed = new List<string>() { "7H" };
+            List<string> tableCards = new List<string>() { "9A" };
 
             // Act
-            var discardResult = _sut.EvaluateCardsOnTable(new List<string>() { "7H" }, new List<string>() { "9A" });
+            var discardResult = _sut.EvaluateCardsOnTable(cardsToBePlayed, tableCards);
 
             // Assert
             Assert.True(discardResult == DiscardResult.Refuse);
         }
 
         [Fact]
-        public void EvaluateCardsOnTable_ShouldReturnResultOk_CardValidToPlay()
+        public void EvaluateCardsOnTable_ShouldReturnResultRefuse_HigherCardThanTableTop()
         {
             // Arrange
-
+            List<string> cardsToBePlayed = new List<string>() { "9H" };
+            List<string> tableCards = new List<string>() { "4A" };
 
             // Act
-            var discardResult = _sut.EvaluateCardsOnTable(new List<string>() { "9H" }, new List<string>() { "9A" });
+            var discardResult = _sut.EvaluateCardsOnTable(cardsToBePlayed, tableCards);
 
             // Assert
             Assert.True(discardResult == DiscardResult.Ok);
         }
 
         [Fact]
-        public void EvaluateCardsOnTable_ShouldReturnResultOk_BurnerCardValidToPlayLowerThan10()
+        public void EvaluateCardsOnTable_ShouldReturnResultOk_SameCardValidToPlay1InHand1InTable()
         {
             // Arrange
-
+            List<string> cardsToBePlayed = new List<string>() { "9H" };
+            List<string> tableCards = new List<string>() { "9A" };
 
             // Act
-            var discardResult = _sut.EvaluateCardsOnTable(new List<string>() { "0H" }, new List<string>() { "9A" });
+            var discardResult = _sut.EvaluateCardsOnTable(cardsToBePlayed, tableCards);
+
+            // Assert
+            Assert.True(discardResult == DiscardResult.Ok);
+        }
+
+        [Fact]
+        public void EvaluateCardsOnTable_ShouldReturnResultOk_SameCardValidToPlay2InHand1InTable()
+        {
+            // Arrange
+            List<string> cardsToBePlayed = new List<string>() { "9H", "9D" };
+            List<string> tableCards = new List<string>() { "9A" };
+
+            // Act
+            var discardResult = _sut.EvaluateCardsOnTable(cardsToBePlayed, tableCards);
+
+            // Assert
+            Assert.True(discardResult == DiscardResult.Ok);
+        }
+
+        [Fact]
+        public void EvaluateCardsOnTable_ShouldReturnResultOkBurned_SameCardValidToPlay3InHand1InTable()
+        {
+            // Arrange
+            List<string> cardsToBePlayed = new List<string>() { "9H", "9D", "9S" };
+            List<string> tableCards = new List<string>() { "9A" };
+
+            // Act
+            var discardResult = _sut.EvaluateCardsOnTable(cardsToBePlayed, tableCards);
 
             // Assert
             Assert.True(discardResult == DiscardResult.OkBurned);
         }
 
         [Fact]
-        public void EvaluateCardsOnTable_ShouldReturnResultOk_BurnerCardValidToPlayHigherThan10()
+        public void EvaluateCardsOnTable_ShouldReturnResultRefused_SameCardInvalidToPlay4OfTheSame()
         {
             // Arrange
-
+            List<string> cardsToBePlayed = new List<string>() { "9H", "9D", "9S", "9A" };
+            List<string> tableCards = new List<string>() { "0A", "KS" };
 
             // Act
-            var discardResult = _sut.EvaluateCardsOnTable(new List<string>() { "0H" }, new List<string>() { "KA" });
+            var discardResult = _sut.EvaluateCardsOnTable(cardsToBePlayed, tableCards);
+
+            // Assert
+            Assert.True(discardResult == DiscardResult.Refuse);
+        }
+
+        [Fact]
+        public void EvaluateCardsOnTable_ShouldReturnResultOkBurned_SameCardValidToPlay4OfTheSame()
+        {
+            // Arrange
+            List<string> cardsToBePlayed = new List<string>() { "9H", "9D", "9S", "9A" };
+            List<string> tableCards = new List<string>() { "2A", "4S" };
+
+            // Act
+            var discardResult = _sut.EvaluateCardsOnTable(cardsToBePlayed, tableCards);
 
             // Assert
             Assert.True(discardResult == DiscardResult.OkBurned);
         }
 
         [Fact]
-        public void EvaluateCardsOnTable_ShouldReturnResultRefuse_CardNotValidToPlay()
+        public void EvaluateCardsOnTable_ShouldReturnResultOk_SameCardValidToPlay1InHand2InTable()
         {
             // Arrange
-
+            List<string> tableCards = new List<string>() { "9H", "9D" };
+            List<string> cardsToBePlayed = new List<string>() { "9A" };
 
             // Act
-            var test = _sut.EvaluateCardsOnTable(new List<string>() { "9H" }, new List<string>() { "5A" });
+            var discardResult = _sut.EvaluateCardsOnTable(cardsToBePlayed, tableCards);
 
             // Assert
-
+            Assert.True(discardResult == DiscardResult.Ok);
         }
 
         [Fact]
-        public void EvaluateCardsOnTable_ShouldReturnResultRefuse_CardNotValidToPlay()
+        public void EvaluateCardsOnTable_ShouldReturnResultOkBurned_SameCardValidToPlay1InHand3InTable()
         {
             // Arrange
-
+            List<string> tableCards = new List<string>() { "9H", "9D", "9S" };
+            List<string> cardsToBePlayed = new List<string>() { "9A" };
 
             // Act
-            var test = _sut.EvaluateCardsOnTable(new List<string>() { "9H" }, new List<string>() { "5A" });
+            var discardResult = _sut.EvaluateCardsOnTable(cardsToBePlayed, tableCards);
 
             // Assert
-
+            Assert.True(discardResult == DiscardResult.OkBurned);
         }
 
         [Fact]
-        public void EvaluateCardsOnTable_ShouldReturnResultRefuse_CardNotValidToPlay()
+        public void EvaluateCardsOnTable_ShouldReturnResultOkBurned_SameCardValidToPlay2InHand2InTable()
         {
             // Arrange
-
+            List<string> cardsToBePlayed = new List<string>() { "9H", "9D" };
+            List<string> tableCards = new List<string>() { "9A", "9C" };
 
             // Act
-            var test = _sut.EvaluateCardsOnTable(new List<string>() { "9H" }, new List<string>() { "5A" });
+            var discardResult = _sut.EvaluateCardsOnTable(cardsToBePlayed, tableCards);
 
             // Assert
+            Assert.True(discardResult == DiscardResult.OkBurned);
+        }
 
+        [Fact]
+        public void EvaluateCardsOnTable_ShouldReturnResultOk_ValidResetCard()
+        {
+            // Arrange
+            List<string> cardsToBePlayed = new List<string>() { "2A" };
+            List<string> tableCards = new List<string>() { "9A", "9C" };
+
+            // Act
+            var discardResult = _sut.EvaluateCardsOnTable(cardsToBePlayed, tableCards);
+
+            // Assert
+            Assert.True(discardResult == DiscardResult.Ok);
+        }
+
+        [Fact]
+        public void EvaluateCardsOnTable_ShouldReturnResultOk_ValidMirrorCard()
+        {
+            // Arrange
+            List<string> cardsToBePlayed = new List<string>() { "3A" };
+            List<string> tableCards = new List<string>() { "9A", "9C" };
+
+            // Act
+            var discardResult = _sut.EvaluateCardsOnTable(cardsToBePlayed, tableCards);
+
+            // Assert
+            Assert.True(discardResult == DiscardResult.Ok);
+        }
+
+        [Fact]
+        public void EvaluateCardsOnTable_ShouldReturnResultOkBurned_BurnerCardValidToPlayWhenLowerThan10()
+        {
+            // Arrange
+            List<string> cardsToBePlayed = new List<string>() { "0H" };
+            List<string> tableCards = new List<string>() { "9A" };
+
+            // Act
+            var discardResult = _sut.EvaluateCardsOnTable(cardsToBePlayed, tableCards);
+
+            // Assert
+            Assert.True(discardResult == DiscardResult.OkBurned);
+        }
+
+        [Fact]
+        public void EvaluateCardsOnTable_ShouldReturnResultOkBurned_BurnerCardValidToPlayWhenHigherThan10()
+        {
+            // Arrange
+            List<string> cardsToBePlayed = new List<string>() { "0H" };
+            List<string> tableCards = new List<string>() { "KA" };
+
+            // Act
+            var discardResult = _sut.EvaluateCardsOnTable(cardsToBePlayed, tableCards);
+
+            // Assert
+            Assert.True(discardResult == DiscardResult.OkBurned);
+        }
+
+        [Fact]
+        public void EvaluateCardsOnTable_ShouldReturnResultRefuse_HigherCardNotValidToPlayWhen7OnTheTable()
+        {
+            // Arrange
+            List<string> cardsToBePlayed = new List<string>() { "9A" };
+            List<string> tableCards = new List<string>() { "7H" };
+
+            // Act
+            var discardResult = _sut.EvaluateCardsOnTable(cardsToBePlayed, tableCards);
+
+            // Assert
+            Assert.True(discardResult == DiscardResult.Refuse);
+        }
+
+        [Fact]
+        public void EvaluateCardsOnTable_ShouldReturnResultRefuse_LowerCardValidToPlayWhen7OnTheTable()
+        {
+            // Arrange
+            List<string> cardsToBePlayed = new List<string>() { "5H" };
+            List<string> tableCards = new List<string>() { "7A" };
+
+            // Act
+            var discardResult = _sut.EvaluateCardsOnTable(cardsToBePlayed, tableCards);
+
+            // Assert
+            Assert.True(discardResult == DiscardResult.Ok);
         }
     }
 }
