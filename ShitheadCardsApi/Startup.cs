e4e35555a4fb8b776a12c9f3513f630aa3fee249
Mauro Schaparini <Mauro.Schaparini@thomsonreuters.com>
2020-05-22 16:08:18 +0100

    Add ignore cors

--- ShitheadCardsApi/Startup.cs
+++ ShitheadCardsApi/Startup.cs
@@ -32,7 +32,7 @@ namespace ShitheadCardsApi
             services.AddSwaggerGen(swagger =>
             {
                 swagger.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Description = "A simple example ASP.NET Core Web API", });
-                
+
                 // Set the comments path for the Swagger JSON and UI.
                 var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
                 var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
@@ -42,7 +42,13 @@ namespace ShitheadCardsApi
             services.AddScoped<IGameService, GameService>();
             services.AddSingleton<IShitheadService, ShitheadService>();
 
-            services.AddCors();
+            services.AddCors(o => o.AddPolicy("CorsDisabled", builder =>
+            {
+                builder.AllowAnyOrigin()
+                       .AllowAnyMethod()
+                       .AllowAnyHeader();
+            }));
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -62,6 +68,8 @@ namespace ShitheadCardsApi
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
             });
+
+            app.UseCors("CorsDisabled");
 
             app.UseRouting();
 
