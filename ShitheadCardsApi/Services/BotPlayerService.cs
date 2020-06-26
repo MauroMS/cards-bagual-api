4f77ced192b8878eeb044483b8a2d6120469b30d
aspich <andriospich@gmail.com>
2020-06-26 22:30:46 +0200

    BotPlayerService init implemented

--- ShitheadCardsApi/Services/BotPlayerService.cs
+++ ShitheadCardsApi/Services/BotPlayerService.cs
@@ -14,6 +14,9 @@ namespace ShitheadCardsApi
     public class BotPlayerService : IBotPlayerService
     {
         private static string[] botPlayerNames = { "John", "Paul", "George", "Ringo" };
+
+        private static string[] bestCards = { "4", "5", "6", "7", "8", "9", "J", "Q", "K", "A", "2", "3", "0" };
+
         private IShitheadService _shitheadService;
 
         public BotPlayerService(IShitheadService shitheadService)
@@ -33,13 +36,30 @@ namespace ShitheadCardsApi
 
         public void InitializePlayer(Game game, string playerName)
         {
-            // TODO: add logic here to set "good" hand cards switching with open ones
+            Player botPlayer = game.Players.Where(p => p.Name.Equals(playerName)).First();
+
+            List<string> handAndOpen = botPlayer.InHandCards.Union(botPlayer.OpenCards).ToList();
+
+            handAndOpen.Sort(CompareBestCard);
+
+            botPlayer.OpenCards = handAndOpen.GetRange(0, 3);
+            botPlayer.InHandCards = handAndOpen.GetRange(3, 3);
+
             game.Players.First(p => p.Name == playerName).Status = StatusEnum.PLAYING;
+        }
+
+        public static int CompareBestCard(string card1, string card2)
+        {
+            int c1pos = Array.IndexOf(bestCards, card1.Substring(0, 1));
+            int c2pos = Array.IndexOf(bestCards, card2.Substring(0, 1));
+
+            return c1pos - c2pos;
         }
 
         public string PlayBotPlayerTurn(Game game)
         {
-            // TODO: implement logic here for bot playing on which card to discard. "null" to collect the table
+            Player botPlayer = game.Players.Where(p => p.Name.Equals(game.PlayerNameTurn)).First();
+
             return null;
         }
 
