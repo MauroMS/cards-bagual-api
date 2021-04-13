06883bda73b8b413a1498be22192e3c85e4e83b1
Mauro Schaparini <Mauro.Schaparini@thomsonreuters.com>
2021-04-13 15:26:38 +0100

    Fix build

--- Bagual.Api/Startup.cs
+++ Bagual.Api/Startup.cs
@@ -3,20 +3,14 @@ using Bagual.Services.Shithead.Interfaces;
 using Bagual.Services.Shithead.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Reflection;
-using System.Threading.Tasks;
 
 namespace Bagual.Api
 {
@@ -74,7 +68,7 @@ namespace Bagual.Api
             app.UseSwaggerUI(c =>
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "Shithead API");
-                c.RoutePrefix = "swagger";
+                c.RoutePrefix = String.Empty;
             });
 
             app.UseCors("CorsDisabled");
