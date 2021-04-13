f9d5158a149fd17ab9ee4c15bcf6b46dc32cb0ef
Mauro Schaparini <mauro.schaparini@gmail.com>
2021-04-13 18:26:48 +0100

    test

--- Bagual.Api/Startup.cs
+++ Bagual.Api/Startup.cs
@@ -39,10 +39,10 @@ namespace Bagual.Api
                 swagger.IncludeXmlComments(xmlPath);
             });
 
-            services.AddScoped<IGameService, GameService>();
-            services.AddSingleton<IShitheadService, ShitheadService>();
-            services.AddScoped<IBotPlayerService, BotPlayerService>();
-            services.AddHostedService<ConsumeBotPlayersService>();
+            //services.AddScoped<IGameService, GameService>();
+            //services.AddSingleton<IShitheadService, ShitheadService>();
+            //services.AddScoped<IBotPlayerService, BotPlayerService>();
+            //services.AddHostedService<ConsumeBotPlayersService>();
 
             services.AddCors(o => o.AddPolicy("CorsDisabled", builder =>
             {
